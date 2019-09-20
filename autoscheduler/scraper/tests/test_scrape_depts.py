71ef71fb5997e492181aa6730b3249e48c8b323d
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-20 17:04:10 -0500

    Outlined test scrape depts more

--- autoscheduler/scraper/tests/test_scrape_depts.py
+++ autoscheduler/scraper/tests/test_scrape_depts.py
@@ -1,8 +1,20 @@
 from django.test import TestCase
+import requests
+import requests_mock
 
 from scraper.management.commands import scrape_depts
 
 class ScrapeDepsTestCase(TestCase):
     def setUp(self):
 
-    def test
+    def parse_departments__emptyInput_doesThrowError(self):
+        """Should throw an error if the json input is empty"""
+        session = requests.Session()
+        adapter = requests_mock.Adapter()
+        session.mount('mock', adapter)
+
+        with requests_mock.mock() as m:
+            m.get(scrape_depts.BASE_URL, text='')
+
+        actual = scrape_depts.get_departments()
+        self.assertEqual('', actual)
