49687bd8971c1b10d5a5916b9b85505aadf8e715
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-20 10:03:18 -0500

    Added scrape_departments outline to scrape_courses

--- scraper/management/commands/scrape_courses.py
+++ scraper/management/commands/scrape_courses.py
@@ -1,6 +1,12 @@
 # Called to execute the beginning of scaping courses
 
 from django.base import base
+import requests
+
+def scrape_departments():
+    """
+    Scrapes the departments 
+    """
 
 class Command(base.Command):
     def handle(self, *args, **options):
