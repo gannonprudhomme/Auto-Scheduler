306853a030c9fdac6aeb3821de293d88c0147d8e
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-20 14:09:23 -0500

    Updated scrape_depts to use parameters

--- autoscheduler/scraper/management/commands/scrape_depts.py
+++ autoscheduler/scraper/management/commands/scrape_depts.py
@@ -5,43 +5,55 @@ from scraper.models import Department
 
 import requests
 import json
+import time
+import datetime
 
-# BASE_URL = 'https://compassxe-ssb.tamu.edu/StudentRegistrationSsb/ssb/classSearch/get_subject'
-BASE_URL = 'https://compassxe-ssb.tamu.edu/StudentRegistrationSsb/ssb/classSearch/get_subject?dataType=json&searchTerm=&term=201931&offset=1&max=500'
+BASE_URL = 'https://compassxe-ssb.tamu.edu/StudentRegistrationSsb/ssb/classSearch/get_subject'
+BASE_URL_PARAMS = 'https://compassxe-ssb.tamu.edu/StudentRegistrationSsb/ssb/classSearch/get_subject?dataType=json&term=201931&offset=1&max=500'
 
 def parse_departments(json):
     """
     Given the JSON of all of the departments, parses them and initializes them into Department objects
     """
 
+    i = 0
     for dept in json:
         # Should have (code, decription)
         dept = Department(code=dept["code"], description=dept["description"])
         dept.save()
+        i = i + 1
+    
+    print(f"Filled {i} departments")
 
     return
 
 # Retrieve all of the departments
 def get_departments():
     """
-    Scrapes the departments and fills the database
+    Sends an GET request to get all of the departments and returns them as JSON
     """
 
     term = '201931' # Get current term from somewhered
-    maxCount = 500
+    maxCount = 300
 
     # Call getsubjects
     params = {
         'dataType': 'json',
         'term': term,
-        'offset': 0,
+        'offset': 1,
         'max': maxCount
     }
 
-    # r = requests.get(BASE_URL, params=params)
-    r = requests.get(BASE_URL)
+    r = requests.get(BASE_URL, params=params)
 
-    return r.json()
+    json = ''
+    # Attempt to convert it to JSON
+    try:
+        json = r.json()
+    except:
+        print('Error converting depts to JSON')
+
+    return json
 
 def scrape_departments():
     json = get_departments()
@@ -51,4 +63,9 @@ def scrape_departments():
 class Command(base.BaseCommand):
     def handle(self, *args, **options):
         # Do stuff
+        start = time.time()
         scrape_departments()
+        end = time.time()
+        seconds_elapsed = int(end - start)
+        time_delta = datetime.timedelta(seconds=seconds_elapsed)
+        print(f"Finished scraping departments in {time_delta}")
