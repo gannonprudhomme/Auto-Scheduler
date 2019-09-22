2a5d3e69ac59e5a01caead74a9839af2f0418d17
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-22 14:15:09 -0500

    Added Instructor model

--- autoscheduler/scraper/models/__init__.py
+++ autoscheduler/scraper/models/__init__.py
@@ -1,4 +1,5 @@
 from .department import Department
 from .course import Course
+from .instructor import Instructor
 
-__all__ = ["Department", "Course"]
+__all__ = ["Department", "Course", "Instructor"]
---
+++ autoscheduler/scraper/models/instructor.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Instructor(models.Model):
+    id = models.CharField(max_length=9, primary_key=True)
+    name = models.TextField(max_length=70)
+    # id = models.CharField(_(""), max_length=50)
