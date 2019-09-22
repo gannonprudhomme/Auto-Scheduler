01b4604e7a325ded91a884b92f6028a9ba0bc787
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-22 15:53:34 -0500

    Updated course model

--- autoscheduler/scraper/models/course.py
+++ autoscheduler/scraper/models/course.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 class Course(models.Model): # Directly from GBS:w
-    id = models.CharField(max_length=10, primary_key=True)
-    dept = models.CharField(max_length=4, db_index=True)
-    course_num = models.CharField(max_length=5, db_index=True)
-    name = models.CharField(max_length=100)
-    description = models.TextField(null=True, blank=True)
-    prerequisites = models.TextField(null=True, blank=True)
-    corequisites = models.TextField(null=True, blank=True)
-    min_credits = models.FloatField(null=True)
-    max_credits = models.FloatField(null=True)
-    distribution_of_hours = models.CharField(max_length=100)
+    id = models.CharField(max_length=10, primary_key=True) # Shouldn't this be a number?
+    dept = models.CharField(max_length=4, db_index=True) # CSCE
+    course_num = models.CharField(max_length=5, db_index=True) # i.e. 221
+    title = models.CharField(max_length=100) # Course title, i.e. "Data Structres & Algorithms"
+    # description = models.TextField(null=True, blank=True)
+    # prerequisites = models.TextField(null=True, blank=True)
+    # corequisites = models.TextField(null=True, blank=True)
+    # min_credits = models.FloatField(null=True)
+    # max_credits = models.FloatField(null=True)
+    # distribution_of_hours = models.CharField(max_length=100)
 
     class Meta:
         db_table: "course"
