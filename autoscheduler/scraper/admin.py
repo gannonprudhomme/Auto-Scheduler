c8962a8a8920f52cb610014c3ccc4ce464e89b27
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-22 23:27:14 -0500

    Added scraper models to admin.py

--- autoscheduler/scraper/admin.py
+++ autoscheduler/scraper/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from scraper.models import Section, Course, Meeting, Instructor, Department
+
+admin.site.register(Section)
+admin.site.register(Course)
+admin.site.register(Meeting)
+admin.site.register(Instructor)
+admin.site.register(Department)
