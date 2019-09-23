ef654e7bf77d5fa79c83f496b038c1cce83dcdcb
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-22 23:19:13 -0500

    Updated section & migrations

--- autoscheduler/scraper/models/section.py
+++ autoscheduler/scraper/models/section.py
@@ -6,8 +6,8 @@ from scraper.models import Course
 # Basically a meeting time that a section meets at
 class Meeting(models.Model):
     id = models.CharField(max_length=20, primary_key=True) # What would this be?
+    crn = models.CharField(max_length=10, db_index=True) # The reference to the section
     building = models.CharField(max_length=4) # Should we reference Building here? If we need it as a model anyways
-    dept = models.CharField(max_length=5)
     meeting_days = models.CharField(max_length=7) # MTWRFSU, where S = saturday, U = Sunday?
     start_time = models.TimeField()
     end_time = models.TimeField()
@@ -15,7 +15,7 @@ class Meeting(models.Model):
     
 
 class Section(models.Model):
-    id = models.CharField(max_length=15, primary_key=True) # CRN? Might be a combo
+    id = models.CharField(max_length=15, primary_key=True) # CRN?
     subject = models.CharField(max_length=4)
     # parent_course = models.ManyToManyField(Course) # The course this is a section of?
 
