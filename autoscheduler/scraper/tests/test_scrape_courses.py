9eb9f513f19e5f8b48256825ea9ebb3856122a2f
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-22 23:19:37 -0500

    Further implemented scrape_courses tests

--- autoscheduler/scraper/tests/test_scrape_courses.py
+++ autoscheduler/scraper/tests/test_scrape_courses.py
@@ -3,7 +3,9 @@ from django.test import TestCase
 import json
 from pathlib import Path
 
-from 
+from scraper.management.commands import scrape_courses
+from scraper.models import Course, Section, Meeting
+from datetime import time
 
 class ScrapeCoursesTestCase(TestCase):
     section_input = {}
@@ -12,14 +14,46 @@ class ScrapeCoursesTestCase(TestCase):
         base_path = Path(__file__).parent
         file_path = (base_path / ".../section_inputs.json").resolve()
 
-        # Load in the section input
+        # Load in the section input test data
         with open(file_path) as json_file:
             data = json.load(json_file)
-            section_input = data['section_test']
+            self.section_input = data['section_test']
             
             json_file.close()
 
-    def test_parse_section__normalInput__formsCorrectly(self):
+    def test__parse_section__normalInput__formsCorrectly(self):
         """ Given a normal section/course input, does it format correctly? """
-        # expected = Section
+        expected = Section(
+            id = "CSCE110-501", 
+        ) 
 
+    def test__parse_meeting__normalInput__formsCorrectly(self):
+        """ Given a normal section meeting input, ensure it formats correctly """
+        expected = Meeting(
+            id = "CSCE110-501-0",
+            crn = "10915",
+            building = "ZACH",
+            meeting_days = "TR",
+            start_time = time(12, 45, 0),
+            end_time = time(14, 0, 0),
+            meeting_type = "LEC"
+        )
+
+        actual = scrape_courses.parse_meeting(self.section_input['meetingsFaculty'][0]['meetingTime'], 'CSCE110-501')
+        print(expected.crn)
+        print(actual.crn)
+
+        self.assertEqual(expected, actual)
+
+    def test__parse_meeting_days__normalInput__formsCorrectly(self):
+        """ Given a normal section meeting input, ensure it formats the days correctly """
+        expected = "TR"
+        actual = scrape_courses.parse_meeting_days(self.section_input['meetingsFaculty'][0]['meetingTime'])
+
+        self.assertEqual(expected, actual)
+
+    def test__parse_time__normalInput__formsCorrectly(self):
+        expected = time(12, 45, 0)
+        actual = scrape_courses.parse_time("1245")
+
+        self.assertEqual(expected, actual)
