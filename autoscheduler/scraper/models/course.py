c72324b7a5bc907130e58ccd54cd7ff97fc3acd9
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-28 12:27:35 -0500

    Updated course & instructor models and migrated

--- autoscheduler/scraper/models/course.py
+++ autoscheduler/scraper/models/course.py
@@ -5,7 +5,7 @@ class Course(models.Model): # Directly from GBS:w
     dept = models.CharField(max_length=4, db_index=True) # CSCE
     course_num = models.CharField(max_length=5, db_index=True) # i.e. 221
     title = models.CharField(max_length=100) # Course title, i.e. "Data Structres & Algorithms"
-    # description = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     # prerequisites = models.TextField(null=True, blank=True)
     # corequisites = models.TextField(null=True, blank=True)
     # min_credits = models.FloatField(null=True)
--- autoscheduler/scraper/models/instructor.py
+++ autoscheduler/scraper/models/instructor.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Instructor(models.Model):
-    id = models.CharField(max_length=9, primary_key=True)
+    id = models.CharField(max_length=9, primary_key=True) # I.e. T00023644
     name = models.TextField(max_length=70)
-    # id = models.CharField(_(""), max_length=50)
+    pidm = models.CharField(max_length=9, null=True) # Another form of their ID, Like 5ish digits
+    email = models.CharField(max_length=50, null=True)
 
     class Meta:
         db_table = "instructors"
