8844880732f01c4391ba625cb501b8f2699d53e3
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-09-28 02:15:23 -0500

    Added fields to Meeting & Section and migrated

--- autoscheduler/scraper/models/section.py
+++ autoscheduler/scraper/models/section.py
@@ -7,10 +7,10 @@ from scraper.models import Course
 class Meeting(models.Model):
     id = models.CharField(max_length=20, primary_key=True) # What would this be?
     crn = models.CharField(max_length=10, db_index=True) # The reference to the section
-    building = models.CharField(max_length=4) # Should we reference Building here? If we need it as a model anyways
+    building = models.CharField(max_length=4, null=True) # Should we reference Building here? If we need it as a model anyways
     meeting_days = models.CharField(max_length=7) # MTWRFSU, where S = saturday, U = Sunday?
-    start_time = models.TimeField()
-    end_time = models.TimeField()
+    start_time = models.TimeField(null=True)
+    end_time = models.TimeField(null=True)
     meeting_type = models.CharField(max_length=50) # Lecture, Lab, etc
 
     class Meta:
@@ -19,11 +19,20 @@ class Meeting(models.Model):
 
 class Section(models.Model):
     id = models.CharField(max_length=15, primary_key=True) # Combination of crn & term
-    subject = models.CharField(max_length=4) # Is this necessary?
-    # Do we need crn?
-    # Do we need term_code?
 
-    # min & max credit(hours)?
+    subject = models.CharField(max_length=4, db_index=True) # Is this necessary?
+    section_num = models.CharField(max_length=6, db_index=True)
+    course_num = models.CharField(max_length=6, db_index=True)
+    term_code = models.IntegerField(db_index=True) # Going to be filtering by term code a lot
+
+    min_credits = models.IntegerField(null=True) # can this be null?
+    max_credits = models.IntegerField(null=True) # Can definitely be null
+
+    # Seating availability?
+    maxEnrollment = models.IntegerField() # Number of students in the class possible
+    currentEnrolled = models.IntegerField() # Can this be null?
+
+    # Wait list stuff? Might be cool to show
 
     instructor = models.CharField(max_length=100) # The instructor's ID. Could this be NULL?
     meetings = models.ManyToManyField(Meeting) # Like an array of meetings?
