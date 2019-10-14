c314ec7aa8573924065f08b4c20bd68e3d51f42d
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-10-14 00:14:26 -0500

    Added DepartmentSearchView for /api/depts/ route

--- autoscheduler/scraper/views.py
+++ autoscheduler/scraper/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import renderers, generics, response
+
+from . import models, serializers
 
 # Create your views here.
+class DepartmentSearchView(generics.ListAPIView):
+    renderer_classes = [renderers.JSONRenderer]
+    serializer_class = serializers.DepartmentSerializer
+
+    # Get 
+    def get_queryset(self):
+        """
+        Returns all of the courses that match the text that was provided
+        Ex: In: "AGS". Out: "AGSC - Agrictulral Science, AGSM - Agriculture Systems Mgmt"
+        """
+
+        dept = self.kwargs["dept"] # The input they've given us. If url was /api/data/AGS, dept would be AGS
+
+        # Check for a cached result? django.core.cache package
+        depts = models.Department.objects.filter(code__istartswith=dept)
+        return depts
