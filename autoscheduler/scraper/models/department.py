dc9fda7cc8bf31e7445c37e6e541697c265aef21
Gannon Prudhomme <gannonprudhomme>
2019-10-25 02:46:49 -0500

    Updated department model and made migrations

--- autoscheduler/scraper/models/department.py
+++ autoscheduler/scraper/models/department.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Department(models.Model):
     # id = models.CharField(max_length=10, primary_key=True) # What does primary_key do?
-    code = models.CharField(max_length=4, primary_key=True)# , db_index=True) # What does db_index do?
+    code = models.CharField(max_length=5, primary_key=True)# , db_index=True) # What does db_index do?
     description = models.TextField(max_length=100)
 
     class Meta:
